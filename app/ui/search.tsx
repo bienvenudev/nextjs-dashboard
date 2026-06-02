afc1a72c9fb1c4aa7041503ce6b120d8d650347e
bienvenudev <cbienvenu007@gmail.com>
2026-06-02 22:25:16 +0200

    feat(search): implement debounced search functionality

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -9,9 +10,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback(term => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     
+    params.set('page', "1");
     if (term) {
       params.set('query', term);
     } else {
@@ -19,7 +23,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params}`)
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
